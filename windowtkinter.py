b97cce72f13e8f8f5a69e2691b9d5bdcc71a1943
ani67h <ghoshanindya4589@gmail.com>
2025-10-04 21:05:50 +0600

    first commit

---
+++ windowtkinter.py
@@ -0,0 +1,10 @@
+# Import necessary libraries
+from tkinter import *
+
+# create window
+window = Tk()
+
+# set the window title and geometry
+window.title('Demo Window')
+window.geometry('400x300')
+window.mainloop()
